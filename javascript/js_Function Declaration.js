393e8f746e9ca53599b8770aa70fe23f26fc606c
chacha <chayeonggeon@gmail.com>
2024-03-08 22:33:37 +0900

    feat: 함수 선언

---
+++ javascript/js_Function Declaration.js
@@ -0,0 +1,24 @@
+// 함수 선언, 함수 선언문 방식
+function sayHi() {
+    alert( "Hello" );
+  }
+
+// 함수 표현식
+let sayHi = function() {
+    alert( "Hello" );
+  };
+
+
+  // 함수 선언문
+function sum(a, b) {
+    return a + b;
+  }
+
+  // 함수 선언문
+function sum(a, b) {
+    return a + b;
+  }
+
+
+// 함수 표현식은 실제 실행 흐름이 해당 함수에 도달했을 때 함수를 생성 -> 실행 흐름이 함수에 도달했을 때부터 해당 함수를 사용가능
+// 함수 선언문은 함수 선언문이 정의되기 전에도 호출할 수 있습니다.
