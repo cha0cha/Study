2f9d79808da0ca67b80b491856bf6ed26b4db36b
chacha <chayeonggeon@gmail.com>
2024-03-10 17:19:43 +0900

    .

---
+++ javascript/signUpPractice/main.js
@@ -0,0 +1,14 @@
+const $userId = document.querySelector('#user_ID');
+const $userIdCheck = document.querySelector('#user_IDcheck');
+const $userPw1 = document.querySelector('#user_PW1');
+const $userPw2 = document.querySelector('#user_PW2');
+const $pwCheck = document.querySelector('#user_PWcheck');
+const $name = document.querySelector('#user_name');
+const $year = document.querySelector('#year');
+const $month = document.querySelector('#month');
+const $day = document.querySelector('#day');
+
+$userIdCheck.addEventListener((click) => {
+
+})
+
