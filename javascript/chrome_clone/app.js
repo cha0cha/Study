31aaf5497213a829ad0f55ce3e2f1a4de95495f6
chacha <chayeonggeon@gmail.com>
2024-03-10 21:54:06 +0900

    update: stage 4

--- javascript/chrome_clone/app.js
+++ javascript/chrome_clone/app.js
@@ -4,16 +4,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting")
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
     // greeting.innerText = "Hello" + username;
-    greeting.innerText = `Hello ${username}`
-    
-    greeting.classList.remove("hidden");
-    
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit",onLoginSubmit)
+function paintGreetings(username){
+    //greeting 텍스트 Hello~ 추가한 후 hidden class명 지워주는 역할
+    greeting.innerText = `Hello ${username}`
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit)
+} else {
+    //show the greetings
+    paintGreetings(savedUsername);
+}
