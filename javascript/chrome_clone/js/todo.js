bba21438a4492df328549e5ded6486cb462830d7
chacha <chayeonggeon@gmail.com>
2024-03-13 23:33:26 +0900

    update: day3

--- javascript/chrome_clone/js/todo.js
+++ javascript/chrome_clone/js/todo.js
@@ -3,11 +3,20 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    console.log(li);
+}
+
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;    //input의 현재 value를 새로운 변수에 복사
     toDoInput.value = "";
-
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
