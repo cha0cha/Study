f57c546afc08fb32b79af095a43abf9881d59dfe
chacha <chayeonggeon@gmail.com>
2024-03-17 23:27:58 +0900

    Update: JSX

--- React/소플의 처음만난 리액트/my-app/src/index.js
+++ React/소플의 처음만난 리액트/my-app/src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Library from './chapter_03/Library';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library />
   </React.StrictMode>
 );
 
