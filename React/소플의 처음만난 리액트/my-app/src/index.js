16851e586573c8dd37c9ab5411658c015f921d3b
chacha <chayeonggeon@gmail.com>
2024-03-24 01:12:51 +0900

    update: 조건부렌더링

--- React/소플의 처음만난 리액트/my-app/src/index.js
+++ React/소플의 처음만난 리액트/my-app/src/index.js
@@ -9,12 +9,13 @@ import reportWebVitals from './reportWebVitals';
 // import CommentList from './chapter_05/CommentList';
 // import NotificationList from './chapter_06/NotificationList';
 // import Accommodate from './chapter_07/Accommodate';
-import ConfirmButton from './chapter_08/ConfirmButton';
+// import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09.jsx/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ConfirmButton />
+    <LandingPage />
   </React.StrictMode>
 );
 // setInterval(() => {
