3cdc5b22d335c1afa1dab65c2ce10872c459ca1e
chacha <chayeonggeon@gmail.com>
2024-03-24 00:55:17 +0900

    update: EventHandling

--- React/소플의 처음만난 리액트/my-app/src/index.js
+++ React/소플의 처음만난 리액트/my-app/src/index.js
@@ -8,12 +8,13 @@ import reportWebVitals from './reportWebVitals';
 // import Clock from './chapter_04/Clock';
 // import CommentList from './chapter_05/CommentList';
 // import NotificationList from './chapter_06/NotificationList';
-import Accommodate from './chapter_07/Accommodate';
+// import Accommodate from './chapter_07/Accommodate';
+import ConfirmButton from './chapter_08/ConfirmButton';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>
 );
 // setInterval(() => {
