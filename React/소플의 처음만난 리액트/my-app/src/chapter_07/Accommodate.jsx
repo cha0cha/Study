0907e84b16064ed872b4e25908f0be5e6126622b
chacha <chayeonggeon@gmail.com>
2024-03-24 00:44:48 +0900

    update: Hook

---
+++ React/소플의 처음만난 리액트/my-app/src/chapter_07/Accommodate.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import useCounter from "./useCounter";
+//rfce
+
+const MAX_CAPACITY = 10;
+function Accommodate(props) {
+  const [isFull, setIsFull] = useState(false);
+  const [count, increaseCount, decreaseCount] = useCounter(0);
+
+  useEffect(() => {
+    console.log("============");
+    console.log("useEffect() is called.");
+    console.log(`isFull: ${isFull}`);
+  });
+
+  useEffect(() => {
+    setIsFull(count >= MAX_CAPACITY);
+    console.log(`Current count value: ${count}`);
+  }, [count]);
+  return (
+    <div style={{ padding: 16 }}>
+      <p>{`총 ${count}명 수용했습니다.`}</p>
+      <button onClick={increaseCount} disabled={isFull}>
+        입장
+      </button>
+      <button onClick={decreaseCount}>퇴장</button>
+
+      {isFull && <p style={{ color: "red" }}> 정원이 가득찼습니다.</p>}
+    </div>
+  );
+}
+
+export default Accommodate;
--- React/소플의 처음만난 리액트/my-app/src/index.js
+++ React/소플의 처음만난 리액트/my-app/src/index.js
@@ -7,12 +7,13 @@ import reportWebVitals from './reportWebVitals';
 // import Library from './chapter_03/Library';
 // import Clock from './chapter_04/Clock';
 // import CommentList from './chapter_05/CommentList';
-import NotificationList from './chapter_06/NotificationList';
+// import NotificationList from './chapter_06/NotificationList';
+import Accommodate from './chapter_07/Accommodate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>
 );
 // setInterval(() => {
