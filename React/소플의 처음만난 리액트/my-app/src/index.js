da70ec4df3380d4cd8e0b02d1d03cfba4454a85d
chacha <chayeonggeon@gmail.com>
2024-03-26 00:52:26 +0900

    update: Shared State

--- React/소플의 처음만난 리액트/my-app/src/index.js
+++ React/소플의 처음만난 리액트/my-app/src/index.js
@@ -12,12 +12,13 @@ import reportWebVitals from './reportWebVitals';
 // import ConfirmButton from './chapter_08/ConfirmButton';
 // import LandingPage from './chapter_09/LandingPage';
 // import AttendanceBook from './chapter_10/AttendanceBook';
-import SignUp from './chapter_11/SignUp';
+// import SignUp from './chapter_11/SignUp';
+import Calculator from './chapter_12/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Calculator />
   </React.StrictMode>
 );
 // setInterval(() => {
