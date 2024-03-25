fdae281b5d069fa8867328023920f24af4247fdb
chacha <chayeonggeon@gmail.com>
2024-03-26 01:09:43 +0900

    update: Containment, Specialization

---
+++ React/소플의 처음만난 리액트/my-app/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+  return (
+    <Card title="Yeonggeon Cha" backgroundColor="#4ea04e">
+      <p>안녕하세요, 차영건입니다.</p>
+      <p>저는 리액트를 공부중 입니다.</p>
+    </Card>
+  );
+}
+
+export default ProfileCard;
